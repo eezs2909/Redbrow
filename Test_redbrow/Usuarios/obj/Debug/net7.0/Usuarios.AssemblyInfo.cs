5cf317076f1184204e608bd343889372c9f5be83
eezs2909 <eduardo.zambrano@gmail.com>
2023-11-24 22:49:46 -0600

    Se sube referencia

--- Test_redbrow/Usuarios/obj/Debug/net7.0/Usuarios.AssemblyInfo.cs
+++ Test_redbrow/Usuarios/obj/Debug/net7.0/Usuarios.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+760a53bcb558f9d063a738c8b405f04350ced910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
